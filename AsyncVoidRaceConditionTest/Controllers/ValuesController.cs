5eb16917e8e64f250b7dd2a6e387f1eac778f86a
Josh Darnell <jdarnell@jswcoinc.com>
2020-12-02 14:04:50 -0500

    Add project files.

---
+++ AsyncVoidRaceConditionTest/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AsyncVoidRaceConditionTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IHttpActionResult Post([FromBody] string value)
+        {
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Entering POST");
+            bool result = RegularMethod();
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Exiting POST");
+            return Json(result);
+        }
+
+        private bool RegularMethod()
+        {
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Entering RegularMethod");
+            AsyncVoidMethod();
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Exiting RegularMethod");
+            return true;
+        }
+
+        private async void AsyncVoidMethod()
+        {
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Entering AsyncVoidMethod");
+            await Task.Delay(2000);
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Exiting AsyncVoidMethod");
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+            Debug.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} Entering PUT");
+        }
+    }
+}
